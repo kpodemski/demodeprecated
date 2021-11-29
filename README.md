f2f0d93a980a752af75987895b48b087c37b8964
Krystian Podemski <podemski.krystian@gmail.com>
2021-11-29 14:47:07 +0100

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+## Demo of "How to handle deprecated methods between versions"
+
+## About
+
+This module demonstrates how you can handle deprecated code between a few versions of the PrestaShop software.
+
+If you need to support multiple versions of the PrestaShop in the single package of your module, this example is for you.
+
+ ### Supported PrestaShop versions
+
+ This module has been tested with PrestaShop 1.6.1.0 and above versions.
+
+ ### Requirements
+
+  1. Composer, see [Composer](https://getcomposer.org/) to learn more
+
+ ### How to install
+
+  1. Download or clone module into `modules` directory of your PrestaShop installation
+  2. Rename the directory to make sure that module directory is named `demodeprecated`*
+  3. `cd` into module's directory and run following commands:
+      - `composer install` - to download dependencies into vendor folder
+  4. Install module from Back Office
+  5. 
+
+ *Because the name of the directory and the name of the main module file must match.
---
+++ src/Adapter/PriceFormatterAdapter.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace PrestaShop\Module\DemoDeprecated\Adapter;
+
+use Currency;
+use PrestaShop\Module\DemoDeprecated\ShopContext;
+use PrestaShop\Module\DemoDeprecated\VersionChecker;
+use Tools;
+
+class PriceFormatterAdapter
+{
+    /**
+     * @var VersionChecker
+     */
+    private $versionChecker;
+
+    /**
+     * @var ShopContext
+     */
+    private $shopContext;
+
+    /**
+     * @var Currency
+     */
+    private $currency;
+
+    public function __construct(
+        ShopContext $shopContext,
+        VersionChecker $versionChecker) {
+        $this->shopContext = $shopContext;
+        $this->currency = $shopContext->getCurrency();
+
+        $this->versionChecker = $versionChecker;
+    }
+
+    public function setCurrency(Currency $currency)
+    {
+        $this->currency = $currency;
+
+        return $this;
+    }
+
+    /**
+     * Tools::displayPrice() is removed in v8, we ned to handle old versions of PrestaShop here
+     * 
+     * @param float $price
+     * 
+     * @return string
+     */
+    public function format($price)
+    {
+        if ($this->versionChecker->is16() || $this->versionChecker->is17()) {
+            return Tools::displayPrice(
+                $price,
+                $this->currency
+            );
+        }
+
+        if ($this->versionChecker->is8()) {
+            return $this->shopContext->getCurrencyLocale()->formatPrice(
+                $price,
+                $this->currency->iso_code
+            );
+        }
+    }
+}
